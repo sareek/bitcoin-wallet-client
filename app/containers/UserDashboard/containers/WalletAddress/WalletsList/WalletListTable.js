586dda8f7ecc501149f0186c55fe187a56718635
Sarik Dhungel <sareek007@gmail.com>
2020-11-06 21:56:03 +0545

    api changes and design change

--- app/containers/UserDashboard/containers/WalletAddress/WalletsList/WalletListTable.js
+++ app/containers/UserDashboard/containers/WalletAddress/WalletsList/WalletListTable.js
@@ -1,45 +1,55 @@
 import React, { Component } from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Table } from 'semantic-ui-react'
 
 
 export class WalletListTable extends Component {
     render() {
-        const { headers, walletAddressesList } = this.props;
+        const { headers, walletAddressesList, getWalletAddressesRequesting } = this.props;
         return (
             <div>
-                 <Table celled>
-                 {headers.length > 0 && (
+                <Table celled>
+                    {headers.length > 0 && (
                         <Table.Header>
-                        <Table.Row>
-                            {headers.map(header => (
-                            <Table.HeaderCell
-                                key={`table_header_${header.key}`}
-                                className={`${header.className || ''}`}
-                            >
-                                {header.name || ''}
-                            </Table.HeaderCell>
-                            ))}
-                        </Table.Row>
+                            <Table.Row>
+                                {headers.map(header => (
+                                    <Table.HeaderCell
+                                        key={`table_header_${header.key}`}
+                                        className={`${header.className || ''}`}
+                                    >
+                                        {header.name || ''}
+                                    </Table.HeaderCell>
+                                ))}
+                            </Table.Row>
                         </Table.Header>
                     )}
-                    <Table.Body>
-                        {walletAddressesList && walletAddressesList.length > 0 ? 
-                       ( <>
-                        { walletAddressesList.map((item, index) => {
-                             return (
-                                <Table.Row key={index}>
-                                <Table.Cell>{item.label}</Table.Cell>
-                                <Table.Cell>{item.balance}</Table.Cell>
+                    {!getWalletAddressesRequesting ? (
+                        <Table.Body>
+                            {walletAddressesList && walletAddressesList.length > 0 ?
+                                (<>
+                                    {walletAddressesList.map((item, index) => {
+                                        return (
+                                            <Table.Row key={index}>
+                                                <Table.Cell>{item.label}</Table.Cell>
+                                                <Table.Cell>{item.balance}</Table.Cell>
+                                            </Table.Row>
+                                        )
+                                    })}
+                                </>
+                                ) : (
+                                    <Table.Row>
+                                        <Table.Cell>No Data</Table.Cell>
+                                    </Table.Row>
+                                )}
+                        </Table.Body>
+                    ) : (
+                            <Table.Body>
+                                <Table.Row>
+                                    <Table.Cell>  <p className="loader_wallet"></p></Table.Cell>
                                 </Table.Row>
-                             )
-                         })}
-                        </>
-                        ) : (
-                            <Table.Row>
-                            <Table.Cell>No Data</Table.Cell>
-                        </Table.Row>
+                            </Table.Body>
+
                         )}
-                    </Table.Body>
+
                 </Table>
             </div>
         )
